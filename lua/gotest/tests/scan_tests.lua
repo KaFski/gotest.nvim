d3ad6435816949370d031668c3301ab61539873f
Pawel Kawecki <kawckipm@gmail.com>
2025-10-17 16:10:20 +0200

    small refactor of test content variable for readability

--- lua/gotest/tests/scan_tests.lua
+++ lua/gotest/tests/scan_tests.lua
@@ -5,30 +5,42 @@ local eq = assert.are.equal
 
 describe("gotest basics", function()
   it("scan tests", function()
-    local lines = {
-      "package sometging",
-      "",
-      "import \"testing\"",
-      "func TestSomethinga(t *testing.T) {",
-      "time.Sleep(time.Millisecond * 100)",
-      "a := \"asdf \"",
-      "if a[0] != 'a' {",
-      "t.Errorf(\"expected % q, but got % q instead \", 'a', a[0])",
-      "}",
-      "",
-      "t.Run(\"test 1\", func(t *testing.T) {",
-      "",
-      "})",
-      "t.Run(\"test 2\", func(t *testing.T) {",
-      "",
-      "})",
-      "}",
-    }
+    local test_content = [[
+package gotestnvim
+
+import (
+	"testing"
+	"time"
+)
+
+func TestSomethinga(t *testing.T) {
+	time.Sleep(time.Millisecond * 100)
+	a := "asdf"
+	if a[0] != 'a' {
+		t.Errorf("expected %q, but got %q instead", 'a', a[0])
+	}
+
+	t.Run("test 1", func(t *testing.T) {
+
+	})
+
+	t.Run("test 2", func(t *testing.T) {
+
+	})
+}
+]]
+
+
+
+    local lines = {}
+    for line in test_content:gmatch("([^\n]+)") do
+      table.insert(lines, line)
+    end
 
     local tests = scanner.scan_tests(lines)
     print("tests:", vim.inspect(tests))
-    eq("^TestSomethinga$", tests[4])
-    eq("^TestSomethinga$/test 1$", tests[11])
+    eq("^TestSomethinga$", tests[6])
+    eq("^TestSomethinga$/test 1$", tests[12])
     eq("^TestSomethinga$/test 2$", tests[14])
   end)
 end)
